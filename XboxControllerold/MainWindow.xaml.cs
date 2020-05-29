2e3419906a3431e623e2c859ef7a2df25f5699e6
PowertrainFSAE47 <dominicis.a@gmail.com>
2020-05-29 14:48:59 -0400

    Inicial

---
+++ XboxControllerold/MainWindow.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Threading;
+using SharpDX.XInput;
+
+namespace XboxController
+{
+
+    public partial class MainWindow : INotifyPropertyChanged
+    {
+        DispatcherTimer _timer = new DispatcherTimer();
+        private string _leftAxis;
+        private string _rightAxis;
+        private string _buttons;
+        private Controller _controller;
+        private string rightTrigger;
+        private string leftTrigger;
+
+        public MainWindow()
+        {
+            DataContext = this;
+            Loaded += MainWindow_Loaded;
+            Closing += MainWindow_Closing;
+            InitializeComponent();
+            _timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(100) };
+            _timer.Tick += _timer_Tick;
+            _timer.Start();
+        }
+
+        void _timer_Tick(object sender, EventArgs e)
+        {
+            DisplayControllerInformation();
+        }
+
+        void DisplayControllerInformation()
+        {
+            byte RT,LT;
+
+            var state = _controller.GetState();
+            
+            RT= state.Gamepad.RightTrigger;
+            LT =state.Gamepad.LeftTrigger;
+            RightTrigger = RT.ToString();
+            LeftTrigger = LT.ToString();
+            LeftAxis = string.Format("X: {0} Y: {1}", state.Gamepad.LeftThumbX, state.Gamepad.LeftThumbY);
+            RightAxis = string.Format("X: {0} Y: {1}", state.Gamepad.RightThumbX, state.Gamepad.RightThumbX);
+            //Buttons = string.Format("A: {0} B: {1} X: {2} Y: {3}", state.Gamepad.Buttons.ToString(), state.Gamepad.LeftThumbY);
+            Buttons = string.Format("{0}", state.Gamepad.Buttons);
+
+        }
+
+        void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            _controller = null;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            _controller = new Controller(UserIndex.One);
+            if (_controller.IsConnected) return;
+            MessageBox.Show("Game Controller is not connected ... you know ;)");
+            App.Current.Shutdown();
+        }
+
+        #region Properties
+
+        public string RightTrigger
+        {
+            get
+            {
+                return rightTrigger;
+            }
+            set
+            {
+                if (value == rightTrigger) return;
+                rightTrigger = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LeftTrigger
+        {
+            get
+            {
+                return leftTrigger;
+            }
+            set
+            {
+                if (value == leftTrigger) return;
+                leftTrigger = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LeftAxis
+        {
+            get
+            {
+                return _leftAxis;
+            }
+            set
+            {
+                if (value == _leftAxis) return;
+                _leftAxis = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string RightAxis
+        {
+            get
+            {
+                return _rightAxis;
+            }
+            set
+            {
+                if (value == _rightAxis) return;
+                _rightAxis = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Buttons
+        {
+            get
+            {
+                return _buttons;
+            }
+            set
+            {
+                if (value == _buttons) return;
+                _buttons = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
